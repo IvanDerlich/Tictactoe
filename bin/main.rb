b06f0cfe6d2ad2eb7ea761fc4af13ea6be79b298
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-07-01 13:59:18 -0300

    Corrected paths in main-rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
-require_relative '../board.rb'
-require_relative '../game.rb'
+require_relative '../lib/board.rb'
+require_relative '../lib/game.rb'
 require_relative 'board_viewer.rb'
 require_relative 'game_viewer.rb'
 
